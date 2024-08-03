ff61659c94b8e7d7eb1e65c696ec36fdd7e38c20
michaljj <mj744@o2.pl>
2024-08-03 20:16:34 +0200

    Added subscribe callback

--- components/mqttHandler/mqttHandler.c
+++ components/mqttHandler/mqttHandler.c
@@ -25,47 +25,54 @@ static void mqttHandler_LogError(const char *message, int error_code)
     }
 }
 
-
-esp_err_t mqttHandler_Publish(const char *data, const char *topic, int qos, int retain)
+static esp_err_t mqttHandler_EventSubsctibedCbk(int MsgId, int CfgMsgId, int CmdMsgId)
 {
-    if (data != NULL && topic != NULL)
+    if (CfgMsgId == MsgId)
     {
-        int msg_id;
-        msg_id = esp_mqtt_client_publish(client, topic, data, 0, qos, retain);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        payload_cha = HomeAssistantHandler_GetDiscovery();
+        if (payload_cha != NULL)
+        {
+            int msg_id;
+            msg_id = esp_mqtt_client_publish(client, TOPIC_CFG, payload_cha, 0, 0, false);
+            ESP_LOGI(TAG, "HA_DISCOVERY_SENT");
+            free(payload_cha);
+            return ESP_OK;
+        }else
+        {
+            return ESP_FAIL;
+        }
+        
+    }else if (CmdMsgId == MsgId)
+    {
+        ESP_LOGI(TAG, "HA_CMD_SUBSCRIBED");
+        return ESP_OK;
+    }else
+    {
+        ESP_LOGI(TAG, "HA_IVALID_TOPIC");
+        return ESP_OK;
     }
-    return ESP_OK;
+    
+    
 }
-
 
 static void mqttHandler_EventHandler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, (int)event_id);
     esp_mqtt_event_handle_t event = event_data;
     esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
+    static int CfgSubMsgId = 0;
+    static int CmdSubMsgId = 0;
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
-        msg_id = esp_mqtt_client_subscribe(client, TOPIC_CFG, 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-        msg_id = esp_mqtt_client_subscribe(client, TOPIC_CMD, 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
+        CfgSubMsgId = esp_mqtt_client_subscribe(client, TOPIC_CFG, 0);
+        CmdSubMsgId = esp_mqtt_client_subscribe(client, TOPIC_CMD, 0);
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
         break;
-
     case MQTT_EVENT_SUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-
-        payload_cha = HomeAssistantHandler_GetDiscovery();
-        if (payload_cha != NULL)
-        {
-            msg_id = esp_mqtt_client_publish(client, TOPIC_CFG, payload_cha, 0, 0, false);
-            free(payload_cha);
-            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-        }
+        ESP_ERROR_CHECK(mqttHandler_EventSubsctibedCbk(event->msg_id, CfgSubMsgId, CmdSubMsgId));
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
@@ -75,8 +82,8 @@ static void mqttHandler_EventHandler(void *handler_args, esp_event_base_t base, 
         break;
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC_CFG=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        ESP_LOGI(TAG, "TOPIC_CFG=%.*s\r\n", event->topic_len, event->topic);
+        ESP_LOGI(TAG, "DATA=%.*s\r\n", event->data_len, event->data);
         break;
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
@@ -92,6 +99,17 @@ static void mqttHandler_EventHandler(void *handler_args, esp_event_base_t base, 
         ESP_LOGI(TAG, "Other event id:%d", event->event_id);
         break;
     }
+}
+
+esp_err_t mqttHandler_Publish(const char *data, const char *topic, int qos, int retain)
+{
+    if (data != NULL && topic != NULL)
+    {
+        int msg_id;
+        msg_id = esp_mqtt_client_publish(client, topic, data, 0, qos, retain);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+    }
+    return ESP_OK;
 }
 
 void mqttHandler_AppStart(void)
